07bb9c20dd3be0a33afa9f87d81f27fcc7ccf7e6
Jane Justiz <janejustiz@gmail.com>
2018-07-29 03:08:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,15 +3,41 @@ class Owner
   @@all = []
 
   def initialize
-
   end
 
   def species
-
   end
 
   def say_species
-
   end
 
+  def name
+  end
+
+  def pets
+  end
+
+  def buy_fish
+  end
+
+  def buy_cat
+  end
+    
+  def buy_dog
+  end
+
+  def walk_dog
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
 end
