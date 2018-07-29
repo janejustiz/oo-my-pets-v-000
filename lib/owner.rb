7cb43c5e1da66f0f95566f2953b6e8b49ad01521
Jane Justiz <janejustiz@gmail.com>
2018-07-29 03:20:07 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,8 +36,9 @@ class Owner
     pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
-  def buy_fish(name,mood = nil)
+  def buy_fish(name, mood = nil)
     fish = Fish.new(name, mood = nil)
+    fish.name = name
   end
 
   def buy_cat
